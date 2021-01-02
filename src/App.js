6ef1525898a66ed7153725e9c6227c8da725dbce
Peter <peterringelmann@gmail.com>
2021-01-02 16:44:45 +0100

    Add interaction VR

--- src/App.js
+++ src/App.js
@@ -1,46 +1,116 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import ReactDOM from 'react-dom'
+import React, {
+  useRef,
+  useState,
+  Suspense,
+  useEffect,
+  useCallback,
+} from 'react'
+import { useFrame, useLoader, useGraph, useThree } from 'react-three-fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { OrbitControls } from '@react-three/drei'
+import {
+  VRCanvas,
+  DefaultXRControllers,
+  useXR,
+  useXREvent,
+  Select,
+} from '@react-three/xr'
+import './index.css'
+import * as THREE from 'three'
 
-function App() {
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
 
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-    </main>
-  );
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
 }
 
-export default App;
+function Asset({ url }) {
+  const scene = useLoader(OBJLoader, url)
+  const { nodes, materials } = useGraph(scene)
+  return (
+    <mesh
+      position={[0, 0, 0]}
+      geometry={nodes.MeshBody1.geometry}
+      material={materials['Steel_-_Satin']}
+    />
+  )
+}
+
+export const App = () => {
+  const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0))
+
+  const mesh = useRef()
+  const { gl, camera } = useThree()
+
+  useEffect(() => {
+    const cam = gl.xr.isPresenting ? gl.xr.getCamera(camera) : camera
+    mesh.current.add(cam)
+    return () => mesh.current.remove(cam)
+  }, [gl.xr.isPresenting, gl.xr, camera, mesh])
+
+  // bundle add the controllers to the same object as the camera so it all stays together.
+  const { controllers } = useXR()
+
+  useEffect(() => {
+    if (controllers.length > 0)
+      controllers.forEach((c) => mesh.current.add(c.grip))
+    return () => controllers.forEach((c) => mesh.current.remove(c.grip))
+  }, [controllers, mesh])
+
+  useFrame(() => {
+    if (controllers.length) {
+    }
+  })
+
+  const onSqueeze = useCallback(() => console.log('Squeezed', controllers[0]), [
+    controllers,
+  ])
+  useXREvent('squeeze', onSqueeze)
+
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      <Select
+        onChange={() => {
+          console.log('SELECTED')
+          setPosition((s) => new THREE.Vector3(10, 10, 10))
+        }}
+      >
+        <Box position={[-1.2, 0, 0]} />
+      </Select>
+
+      <mesh position={position} ref={mesh}></mesh>
+
+      <Suspense fallback={<Box position={[4, 0, 0]} />}>
+        <Asset url="/HOME.obj" />
+      </Suspense>
+      <DefaultXRControllers />
+      <OrbitControls />
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,59 +1,12 @@
-import ReactDOM from "react-dom";
-import React, { useRef, useState, Suspense } from "react";
-import {  useFrame, useLoader, useGraph } from "react-three-fiber";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { OrbitControls } from '@react-three/drei'
-import { VRCanvas, DefaultXRControllers } from '@react-three/xr'
+import ReactDOM from 'react-dom'
+import React from 'react'
 import './index.css'
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
-
-function Asset({ url }) {
-  const scene = useLoader(OBJLoader, url);
-  console.log("obj", scene);
-  const { nodes, materials } = useGraph(scene);
-  console.log("nodes", nodes, materials);
-  return <mesh position={[0,0,0]} geometry={nodes.MeshBody1.geometry} material={materials["Steel_-_Satin"]} />
-}
+import { App } from './App'
+import { VRCanvas } from '@react-three/xr'
 
 ReactDOM.render(
   <VRCanvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-
-    <Suspense fallback={<Box position={[4, 0, 0]}  />}>
-      <Asset url="/HOME.obj" />
-    </Suspense>
-    <OrbitControls />
-    <DefaultXRControllers />
+    <App />
   </VRCanvas>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
